271012c6f9576779c7a7807943c2a7e557f8cfc3
Graham King <graham.king@netwrix.com>
2024-12-06 11:29:47 +0000

    Fix formatting
    Fix formatting 2

--- 2024/Day01/Solution.cs
+++ 2024/Day01/Solution.cs
@@ -8,18 +8,20 @@ using System.Text.RegularExpressions;
 using System.Text;
 
 [ProblemName("Historian Hysteria")]
-class Solution : Solver {
+class Solution : Solver
+{
 
     public object PartOne(string input) =>
-        Enumerable.Zip(GetOrderedColumn(input, 0),GetOrderedColumn(input, 1))
-            .Select(p=> Math.Abs(p.First - p.Second))
+        Enumerable.Zip(GetOrderedColumn(input, 0), GetOrderedColumn(input, 1))
+            .Select(p => Math.Abs(p.First - p.Second))
             .Sum();
 
-    public object PartTwo(string input) {
+    public object PartTwo(string input)
+    {
         var left = GetColumn(input, 0);
         var rightMap = GetColumn(input, 1).CountBy(x => x).ToDictionary();
 
-        var similarityScore = left.Sum(x=> rightMap.TryGetValue(x, out var result) ? x * result : 0);
+        var similarityScore = left.Sum(x => rightMap.TryGetValue(x, out var result) ? x * result : 0);
 
         return similarityScore;
     }
@@ -27,9 +29,10 @@ class Solution : Solver {
     private IReadOnlyCollection<int> GetColumn(string input, int columnIndex)
     {
         var lines = input.Split('\n');
-        var numbers = lines.Select(l=> int.Parse(l.Split("   ")[columnIndex]));
+        var numbers = lines.Select(l => int.Parse(l.Split("   ")[columnIndex]));
         return numbers.ToArray();
     }
+    
     private IReadOnlyCollection<int> GetOrderedColumn(string input, int columnIndex)
     {
         return GetColumn(input, columnIndex).Order().ToArray();
