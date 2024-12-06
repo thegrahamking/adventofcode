d67f04cafdc9414ec033f87c7e51ae0562a8cb41
Graham King <graham.king@gmail.com>
2024-12-06 21:44:10 +0000

    2024 Day 3 Part 2 complete

--- 2024/Day03/Solution.cs
+++ 2024/Day03/Solution.cs
@@ -6,19 +6,53 @@ using System.Collections.Immutable;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Text;
+using AngleSharp.Common;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
 
 [ProblemName("Mull It Over")]
 class Solution : Solver
 {
-    private static readonly Regex regex = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)");
+    private static readonly Regex mulFinder = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)");
 
-    public object PartOne(string input)
-    {
-        return regex.Matches(input).Sum(m => int.Parse(m.Groups[1].Value) * int.Parse(m.Groups[2].Value));
-    }
+    public object PartOne(string input) => mulFinder.Matches(input).Sum(Multiply);
 
     public object PartTwo(string input)
     {
-        return 0;
+        const string enabler = "do()";
+        const string disabler = "don't()";
+
+        return
+            // Find mul operation, or enablers or disablers
+            new Regex(@$"{mulFinder}|{Regex.Escape(enabler)}|{Regex.Escape(disabler)}")
+                .Matches(input)
+                .Aggregate(
+                    // Start off with operations enabled and zero sum
+                    (isEnabled: true, total: 0L),
+                    (accumulator, match) =>
+
+                        (match.Value, accumulator.total, accumulator.isEnabled) switch
+                        {
+                            // Enabler found - Update the accumulator to enable operations (leave total unchanged)
+                            (enabler, _, _) => (true, accumulator.total),
+
+                            // Disabler found - Update the accumulator to disable operations (leave total unchanged)
+                            (disabler, _, _) => (false, accumulator.total),
+
+                            // Operation found - Update actuator total (leave operations enabled)
+                            (_, var total, true) => (true, total + Multiply(match)),
+
+                            // Unknown found - Continue
+                            _ => accumulator
+                        },
+
+                        // Return the accumulated total
+                        accumulator => accumulator.total
+                    );
+    }
+
+    private static int Multiply(Match m)
+    {
+        return int.Parse(m.Groups[1].Value) * int.Parse(m.Groups[2].Value);
     }
 }
