e81a5b50331cde5e3c6a39027ecd8cbb04812834
Graham King <graham.king@netwrix.com>
2024-12-06 11:59:40 +0000

    2024 Day 2 Part 2 complete

--- 2024/Day02/Solution.cs
+++ 2024/Day02/Solution.cs
@@ -11,25 +11,35 @@ using System.Text;
 class Solution : Solver
 {
 
-    public object PartOne(string input)
-    {
-        var reports = input
-                        .Split('\n')
-                        .Select(r => r.Split(' ').Select(l => int.Parse(l)).ToArray())
-                        .ToArray();
+    public object PartOne(string input) => ParseReports(input).Count(IsSafe);
 
-        return reports.Count(levels =>
-        {
-            var pairs = Enumerable.Zip(levels, levels.Skip(1));
-            return
-                pairs.All(p => p.Second - p.First >= 1 && p.Second - p.First <= 3) ||
-                pairs.All(p => p.First - p.Second >= 1 && p.First - p.Second <= 3);
-        });
+    public object PartTwo(string input) => ParseReports(input).Count(levels => GeneratePotentiallySafeVariations(levels).Any(x => IsSafe(x)));
+
+    private static int[][] ParseReports(string input)
+    {
+        return input.Split('\n')
+                    .Select(r => r.Split(' ').Select(l => int.Parse(l)).ToArray())
+                    .ToArray();
     }
 
+    private static int[][] GeneratePotentiallySafeVariations(IReadOnlyCollection<int> levels) =>
+        Enumerable
+            .Range(0, levels.Count)
+            .Select(i =>
+            {
+                var before = levels.Take(i);
+                var after = levels.Skip(i + 1).Take(levels.Count - i - 1);
+                return before.Concat(after).ToArray();
+            })
+            .ToArray();
 
-    public object PartTwo(string input)
+
+
+    private static bool IsSafe(IReadOnlyCollection<int> levels)
     {
-        return 0;
+        var pairs = Enumerable.Zip(levels, levels.Skip(1));
+        return
+            pairs.All(p => p.Second - p.First >= 1 && p.Second - p.First <= 3) ||
+            pairs.All(p => p.First - p.Second >= 1 && p.First - p.Second <= 3);
     }
 }
